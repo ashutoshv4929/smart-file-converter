b3b7d9ef0c8f0908bbb9c3de29b7b73183805359
Ashutosh Verma <ashutoshv4929@gmail.com>
2025-07-09 12:28:51 +0530

    Added vite.config.ts and fixed build issues

---
+++ client/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'fs', 'path', 'http', 
+        'child_process', 'crypto',
+        'os', 'stream', 'zlib'
+      ]
+    }
+  }
+})
